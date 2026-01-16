d775194ffc2fc777b73afc74b39c0014adf80d58
Sam Jackson <hipposareevil@gmail.com>
2026-01-16 17:54:18 +1300

    added calibre in 'more' menu

--- bin/sync_to_wpff.sh
+++ bin/sync_to_wpff.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-rsync -azvv -e 'ssh -p 66' public/ web\@willprogramforfood.com:/var/web_root/blog/
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+rsync -azvv \
+      -e 'ssh -p 66' \
+       --exclude-from=${SCRIPT_DIR}/rsync-exclude.txt \
+      public/  web\@willprogramforfood.com:/var/web_root/blog/
